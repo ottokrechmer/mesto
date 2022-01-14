2aa8e1a1524a140d74d29af086c1d701129b21fb
Затуловский Вадим Аркадьевич <Zatulovskiy.V.Ar@sberbank.ru>
2022-01-14 14:18:24 +0300

    feat: js logic with popup

---
+++ script.js
@@ -0,0 +1,31 @@
+let content = document.querySelector('.content');
+let username = content.querySelector('#profile__name')
+let userDescription = content.querySelector('.profile__description')
+let editButton = content.querySelector('.profile__button_type_edit')
+let popup = content.querySelector('.popup')
+let popupCloseButton = popup.querySelector('.popup__close-button')
+let popupSubmitButton = popup.querySelector('.popup__submit-button')
+let usernameInput = popup.querySelector('#name')
+let descriptionInput = popup.querySelector('#description')
+
+function changePopupVisibility () {
+    popup.classList.toggle('popup_opened')
+}
+
+function openPopup () {
+    usernameInput.value = username.textContent
+    descriptionInput.value = userDescription.textContent
+
+    changePopupVisibility()
+}
+
+function editProfile () {
+    username.textContent = usernameInput.value
+    userDescription.textContent = descriptionInput.value
+
+    changePopupVisibility()
+}
+
+editButton.addEventListener('click', openPopup)
+popupCloseButton.addEventListener('click', changePopupVisibility)
+popupSubmitButton.addEventListener('click', editProfile)
